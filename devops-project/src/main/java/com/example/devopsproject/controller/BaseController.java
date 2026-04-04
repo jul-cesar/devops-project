bbc329507a62b669b703eb8b655d6955966e569e
Julio Cesar Martinez <juliocesarmartinez1344@gmail.com>
2026-04-04 13:43:00 -0500

    artefactos backend spring

---
+++ devops-project/src/main/java/com/example/devopsproject/controller/BaseController.java
@@ -0,0 +1,23 @@
+package com.example.devopsproject.controller;
+
+import com.example.devopsproject.dto.BaseResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/devops")
+public class BaseController {
+
+    @GetMapping
+    public ResponseEntity<BaseResponse> Test() {
+
+        BaseResponse res = new BaseResponse(
+                200,
+                "Hola"
+        );
+        return ResponseEntity.ok(res);
+    }
+}
